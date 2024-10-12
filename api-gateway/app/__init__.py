3b73a0123caa7de876cd557bf431026ad2d67371
Jeremias Ocaña <jere.oca@hotmail.com>
2024-10-12 06:18:00 -0300

    Modificaciones a api-gateway

--- api-gateway/app/__init__.py
+++ api-gateway/app/__init__.py
@@ -1,15 +1,13 @@
 #api-gateway/app/__init__.py
 from flask import Flask
-from .routes import api_gateway
+from routes import api_gateway
 
 def create_app():
     app = Flask(__name__)
     
-    
     app.config['SECRET_KEY'] = 'supersecretkey'
     app.config['DEBUG'] = True
 
-    
     app.register_blueprint(api_gateway)
 
     return app
---
+++ api-gateway/app/services.py
@@ -0,0 +1,51 @@
+import requests
+from saga import SagaBuilder, SagaError
+from saga_order import *
+from flask import jsonify
+
+# Crear la saga ('context' será el conjunto de datos obtenidos al solicitar la creación de la orden)
+def build_saga(saga_context):
+    return SagaBuilder.create() \
+        .action(
+            lambda: add_purchase(saga_context['product_id'], saga_context['purchase_address']),
+            # ---
+            lambda: remove_purchase(saga_context['product_id'], saga_context['purchase_address'])
+        ) \
+        .action(
+            lambda: register_payment(saga_context['user_id'], saga_context['total']),
+            lambda: remove_payment(saga_context['user_id'], saga_context['total'])
+        ) \
+        .action(
+            lambda: add_stock(saga_context['product_id']),
+            lambda: remove_stock(saga_context['product_id'])
+        .action(
+            lambda: success()
+        )
+        ) \
+        .build()
+
+def execute_saga(saga):
+    # Ejecutar la Saga
+    try:
+        saga.execute()
+        return jsonify({"message": "Pedido procesado con éxito"}), 200
+    # Caso de error de saga
+    except SagaError as e:
+        # Si ocurre un error, se manejan las compensaciones automáticamente
+        return jsonify({
+            "error": str(e.action),
+            "compensation_errors": [str(comp_error) for comp_error in e.compensations]
+        }), 400
+    # Caso de otra excepción
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+def handle_response(add_purchase_url, purchase_data):
+
+    try:
+        response = requests.post(add_purchase_url, json=purchase_data)
+        response.raise_for_status()  # Lanza una excepción si el código de estado es 4xx o 5xx
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        # Lanza una excepción para que saga-py inicie la compensación
+        raise Exception(f"Error al realizar la compra: {str(e)}")
