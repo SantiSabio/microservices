78c5375b4179800b4f479a258291725ac1289665
Jeremias Ocaña <jere.oca@hotmail.com>
2024-10-13 03:31:52 -0300

    Update routes.py in ms-payment

--- ms-payment/app/routes.py
+++ ms-payment/app/routes.py
@@ -10,8 +10,8 @@ def add_payment():
     # Se reciben los datos de pago
     data = request.get_json()
     # Validar que los datos necesarios estén presentes
-    
-    required_fields = ['product_id', 'quantity', 'price', 'payment_method']
+    required_fields = ['product_id', 'price', 'payment_method']
+
     if not all(field in data for field in required_fields):
         return jsonify({'error': 'Missing fields'}), 400
     
@@ -38,27 +38,22 @@ def add_payment():
 def remove_payment():
     # Se reciben los datos de pago a borrar
     data = request.get_json()
-    # Validar que los datos necesarios estén presentes
     
-    required_fields = ['product_id', 'quantity', 'price', 'payment_method']
-    if not all(field in data for field in required_fields):
+    if not 'payment_id' in data:
         return jsonify({'error': 'Missing fields'}), 400
     
     try:
-        # Crea un nuevo pago a borrar
-        old_payment = Payment(
-            product_id=data['product_id'],
-            quantity=data['quantity'],
-            price=data['price'],
-            purchase_id=data['purchase_id'],
-            payment_method=data['payment_method']
-        )
-
-        # Agregar nuevo pago a la base de datos
-        db.session.add(old_payment)
+        # Buscar el pago
+        old_payment = Payment.query.get(data['payment_id'])
+        
+        if not old_payment:
+            return jsonify({'error': 'Payment not found'}), 404
+        
+        # Eliminar el pago encontrado
+        db.session.delete(old_payment)
         db.session.commit()
 
-        return jsonify({'message': 'Payment removed successfully'}), 201
+        return jsonify({'message': 'Payment removed successfully'}), 200
     except Exception as e:
         db.session.rollback()  # Hacer rollback en caso de error
         return jsonify({'error': str(e)}), 500
