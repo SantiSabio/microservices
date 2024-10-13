81de42e651add6c17585895293b64b21ee6ecca3
Jeremias Ocaña <jere.oca@hotmail.com>
2024-10-13 03:30:44 -0300

    Update saga_order.py

--- api-gateway/app/saga_order.py
+++ api-gateway/app/saga_order.py
@@ -14,14 +14,11 @@ from services import response_from_url
 # TODO: Refactoizar
 def add_purchase(product_id, address):
     # Recibir datos de compra en json, y colocarlos en el contexto
-    # purchase_data = request.get_json()
     purchase_data = {
         'product_id': product_id,
         'address': address
     }
-    
-    # Enviar 'purchase_data' al microservicio 'ms-purchase' (solicitud POST)
-    # TODO: Recibir 'purchase_id'
+    # Enviar al microservicio
     add_purchase_url = 'http://ms-purchase:5002/purchase/add'
 
     return response_from_url(add_purchase_url, purchase_data) # Será 'purchase_id' o una excepción
@@ -36,47 +33,56 @@ def remove_purchase(purchase_id):
     # Enviar 'data' al microservicio 'ms-purchase' (solicitud POST)
     remove_purchase_url = 'http://ms-purchase:5002/purchase/remove'
 
-    return response_from_url(remove_purchase_url, purchase_data)
-
+    return response_from_url(remove_purchase_url, purchase_data) # Será 200 o una excepción
 
 # Paso 2: Enviar los datos de pago a ms-payment
 # ---
-def register_payment():
+def add_payment(product_id, pay_method):
     # Recibir los datos de pago
-    payment_data = request.get_json()
+    payment_data = {
+        'product_id': product_id,
+        'pay_method': pay_method
+    }
     
-    # Enviar al microservicio
     add_payment_url = 'http://ms-payment:5004/payment/add'
     
     response_from_url(add_payment_url, payment_data)
 
-def remove_payment():
+def remove_payment(payment_id):
     # Recibir los datos de pago
-    data = request.get_json()
+    payment_data = {
+        'payment_id': payment_id
+    }
     
     # Enviar al microservicio
     remove_payment_url = 'http://ms-payment:5004/payment/remove'
 
-    return response_from_url(remove_payment_url, data)
+    return response_from_url(remove_payment_url, payment_data)
 
 # Paso 3: Registrar strock
-def add_stock():
-    # Recibir los datos de pago
-    data = request.get_json()
+def update_stock(product_id, ammount, in_out):
+    # Recibir los datos de actualización de stock
+    stock_data = {
+        'product_id': product_id,
+        'ammount': ammount,
+        'in_out' : in_out
+    }
     
     # Enviar al microservicio
-    add_stock_url = 'http://ms-inventory:5004/stock/add'
+    add_stock_url = 'http://ms-inventory:5003/inventory/update'
 
-    return response_from_url(add_stock_url, data)
+    return response_from_url(add_stock_url, stock_data)
 
-def remove_stock():
-    # Recibir los datos de pago
-    data = request.get_json()
-
+def remove_stock(stock_id):
+    
+    stock_data = {
+        'stock_id': stock_id
+    }
+    
     # Enviar al microservicio
-    remove_stock_url = 'http://ms-inventory:5004/stock/remove'
+    remove_stock_url = 'http://ms-inventory:5003/inventory/remove'
 
-    return response_from_url(remove_stock_url, data)
+    return response_from_url(remove_stock_url, stock_data)
 
 # Paso 4: Saga completada
 def success():
