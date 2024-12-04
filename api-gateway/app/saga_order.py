2ef7da7107d1ec90bb645e63ccef7f7e8bcbc067
SantiSabio <santiagomartinsabio@gmail.com>
2024-12-04 02:03:16 -0300

    correccion

--- api-gateway/app/saga_order.py
+++ api-gateway/app/saga_order.py
@@ -2,29 +2,29 @@ from flask import jsonify
 from app.utils import response_from_url
 
 # Paso 1: Enviar los datos de compra a ms-purchase
-def agregar_compra(product_id, direccion_compra):
-    datos_compra = {
+def add_purchase(product_id, purchase_direction):
+    purchase_data = {
         'product_id': product_id,
-        'purchase_direction': direccion_compra
+        'purchase_direction': purchase_direction
     }
-    url_agregar_compra = 'http://ms-purchase:5002/purchase/add'
+    add_purchase_url = 'http://ms-purchase:5002/purchase/add'
 
-    response = response_from_url(url_agregar_compra, datos_compra)
+    response = response_from_url(add_purchase_url, purchase_data)
     
     if response['status_code'] == 201:
-        id_compra = response.get('id_purchase')
-        return {'purchase_id': id_compra}
+        id_purchase = response.get('id_purchase')
+        return {'purchase_id': id_purchase}
     else:
         raise Exception(f"Error al realizar el pago: {response['status_code']} {response.get('text', '')}")
 
-# Compensación
-def remover_compra(id_compra):
-    datos_compra = {
-        'id_purchase': id_compra
+# Compensacion
+def remove_purchase(id_purchase):
+    purchase_data = {
+        'id_purchase': id_purchase
     }
-    url_remover_compra = 'http://ms-purchase:5002/purchase/remove'
+    remove_purchase_url = 'http://ms-purchase:5002/purchase/remove'
 
-    response = response_from_url(url_remover_compra, datos_compra)
+    response = response_from_url(remove_purchase_url, purchase_data)
     
     if response['status_code'] == 200:
         return response
@@ -32,51 +32,51 @@ def remover_compra(id_compra):
         raise Exception(f"Error al remover la compra: {response['status_code']} {response.get('text', '')}")
 
 # Paso 2: Enviar los datos de pago a ms-payment
-def agregar_pago(product_id, precio, metodo_pago):
-    datos_pago = {
+def add_payment(product_id, price, payment_method):
+    payment_data = {
         'product_id': product_id,
-        'price': precio,
-        'payment_method': metodo_pago
+        'price': price,
+        'payment_method': payment_method
     }
-    url_agregar_pago = 'http://ms-payment:5004/payment/add'
+    add_payment_url = 'http://ms-payment:5004/payment/add'
     
-    response = response_from_url(url_agregar_pago, datos_pago)
+    response = response_from_url(add_payment_url, payment_data)
     
     return response
 
-def remover_pago(payment_id):
-    datos_pago = {
+def remove_payment(payment_id):
+    payment_data = {
         'payment_id': payment_id
     }
-    url_remover_pago = 'http://ms-payment:5004/payment/remove'
+    remove_payment_url = 'http://ms-payment:5004/payment/remove'
 
-    response = response_from_url(url_remover_pago, datos_pago)
+    response = response_from_url(remove_payment_url, payment_data)
     
     return response
 
 # Paso 3: Registrar stock
-def actualizar_stock(product_id, cantidad, entrada_salida):
-    datos_stock = {
+def update_stock(product_id, ammount, in_out):
+    stock_data = {
         'product_id': product_id,
-        'ammount': cantidad,
-        'in_out': entrada_salida
+        'ammount': ammount,
+        'in_out': in_out
     }
-    url_actualizar_stock = 'http://ms-inventory:5003/inventory/update'
+    add_stock_url = 'http://ms-inventory:5003/inventory/update'
 
-    response = response_from_url(url_actualizar_stock, datos_stock)
+    response = response_from_url(add_stock_url, stock_data)
     
     return response
 
-def remover_stock(stock_id):
-    datos_stock = {
+def remove_stock(stock_id):
+    stock_data = {
         'stock_id': stock_id
     }
-    url_remover_stock = 'http://ms-inventory:5003/inventory/remove'
+    remove_stock_url = 'http://ms-inventory:5003/inventory/remove'
 
-    response = response_from_url(url_remover_stock, datos_stock)
+    response = response_from_url(remove_stock_url, stock_data)
     
     return response
 
 # Paso 4: Saga completada
-def exito():
+def success():
     return jsonify({'message': "Compra realizada con éxito."}), 201
