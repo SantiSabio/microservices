24ea418f3e4cab0da1caccb9bf36abe6a949e3a9
Jeremias Ocaña <129911855+jere-oca@users.noreply.github.com>
2025-03-06 14:56:16 -0300

    Update services.py

--- ms-inventory/app/__init__.py
+++ ms-inventory/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-
 from app.config import Config
 from app.routes import inventory_bp
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
--- ms-inventory/app/config.py
+++ ms-inventory/app/config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     API_GATEWAY_URL = os.getenv('API_GATEWAY_URL')
