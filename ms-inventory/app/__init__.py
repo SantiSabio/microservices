1621504bdc24e3376f31f8b6f5872fbe0e4bc662
Lautaro Sanz <44878724@alumnos.frsr.utn.edu.ar>
2025-03-06 17:40:09 -0300

    payments

--- ms-inventory/app/__init__.py
+++ ms-inventory/app/__init__.py
@@ -6,5 +6,5 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     app.register_blueprint(inventory_bp, url_prefix='/inventory')
-
+    app.config['DEBUG'] = True
     return app
